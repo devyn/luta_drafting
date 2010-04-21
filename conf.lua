30482689e7ceecb29dbc4f5c08128f0c42ed8ed1
devyn <devyn.cairns@gmail.com>
2010-04-21 15:21:12 -0700

    AWESOME! Parallax formula 100% working! (lol, it's a = m * ((s/2 - c/2) - (w/2 - c/2)), can't believe I didn't think of it before...

--- conf.lua
+++ conf.lua
@@ -1,3 +1,6 @@
 function love.conf(t)
   t.title = "Luta Drafting"
+  --t.screen.fullscreen = true
+  t.screen.width = 800
+  t.screen.height = 600
 end
--- object/parallax_background.lua
+++ object/parallax_background.lua
@@ -2,20 +2,10 @@ function ParallaxBackground(fn)
   return {
     img = LutaImage(fn),
     draw = function (self, scene)
-      local camera = scene.camera
-      local scx = self.img.aw / scene.width
-            scx = scx - (1.0 - scx)
-      local scy = self.img.ah / scene.height
-            scy = scy - (1.0 - scy)
-      local frx = scene.width / 2
-      local fry = scene.height / 2
-      local dsx = self.img.aw / 2
-      local dsy = self.img.ah / 2
-      local ccx = camera.x + (camera.w / 2)
-      local ccy = camera.y + (camera.h / 2)
-      local pox = (ccx - frx) * scx + dsx - (camera.w / 2)
-      local poy = (ccy - fry) * scy + dsy - (camera.h / 2)
-      self.img:draw({x=pox,y=poy,w=camera.w,h=camera.h}, -pox, -poy)
+      local cam = scene.camera
+      local drx = (self.img.aw/2 - cam.w/2) / (scene.width/2 - cam.w/2)
+      local dry = (self.img.ah/2 - cam.h/2) / (scene.height/2 - cam.h/2)
+      self.img:draw({x=cam.x*drx,y=cam.y*dry,w=cam.w,h=cam.h}, -cam.x*drx, -cam.y*dry)
     end
   }
 end
